482225a5e1f10a2985aecb41a581446ce893a340
Harley Alexander <harley@mayte.io>
2020-02-17 14:24:34 +1100

    temporarily use git as dependency

--- example/example.js
+++ example/example.js
@@ -1,14 +1,20 @@
-import React from "react";
-import { render } from "react-dom";
-import { useLocalStorage } from "../";
+import React from 'react';
+import { render } from 'react-dom';
+import { useLocalStorage } from '../';
 
 const App = () => {
-  let name = useLocalStorage("name");
+  const [key, set] = React.useState('key1');
+  let [name, setName] = useLocalStorage(key, key === 'key1' ? 'default value' : 'default value 2');
+  React.useEffect(() => {
+    setName('key1 name');
+    setTimeout(() => set('key2'), 1000);
+  }, []);
+
   return (
     <div>
       <h1>{name}</h1>
     </div>
   );
-}
+};
 
 render(<App />, document.getElementById('root'));
--- src/use-localstorage.ts
+++ src/use-localstorage.ts
@@ -60,7 +60,7 @@ export function useLocalStorage<TValue = string>(
         }
       }
     },
-    [updateLocalState, key]
+    [updateLocalState]
   );
 
   useEffect(() => {
